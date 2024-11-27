2250d872d2320bcf4d9af65bf42d7a729bec7378
Braiton2117 <richarctm@gmail.com>
2024-11-27 10:15:38 -0500

    Update reservas.routes.js

--- src/routes/reservas.routes.js
+++ src/routes/reservas.routes.js
@@ -4,11 +4,11 @@ import { verifyToken } from '../jwt/verifyToken.js';
 const router=Router()
 //armar nuestras rutas
 
-router.get('/reservas',verifyToken,getReservas)//select 
-router.get('/reservas/:id',verifyToken, getReservaxid)//select con id
-router.post('/reservas',verifyToken, postReserva)//insert
-router.put('/reservas/:id',verifyToken,putReserva) //update
-router.patch('/reservas/:id',verifyToken,patchReserva)  //update
-router.delete('/reservas/:id',verifyToken,deleteReserva)
+router.get('/reservas',getReservas)//select 
+router.get('/reservas/:id', getReservaxid)//select con id
+router.post('/reservas', postReserva)//insert
+router.put('/reservas/:id',putReserva) //update
+router.patch('/reservas/:id',patchReserva)  //update
+router.delete('/reservas/:id',deleteReserva)
 
 export default router
