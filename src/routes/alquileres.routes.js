8a41d0a65a231a68b762361280790f1da83dd08c
Braiton2117 <richarctm@gmail.com>
2024-11-27 10:17:35 -0500

    Update alquileres.routes.js

--- src/routes/alquileres.routes.js
+++ src/routes/alquileres.routes.js
@@ -5,11 +5,11 @@ import { verifyToken } from '../jwt/verifyToken.js';
 const router=Router()
 //armar nuestras rutas
 
-router.get('/alquileres',verifyToken,getAlquileres)//select 
-router.get('/alquileres/:id',verifyToken, getAlquilerxid)//select con id
-router.post('/alquileres',verifyToken, postAlquiler)//insert
-router.put('/alquileres/:id',verifyToken,putAlquiler) //update
-router.patch('/alquileres/:id',verifyToken,patchAlquiler) //update
-router.delete('/alquileres/:id',verifyToken,deleteAlquiler)
+router.get('/alquileres',getAlquileres)//select 
+router.get('/alquileres/:id', getAlquilerxid)//select con id
+router.post('/alquileres', postAlquiler)//insert
+router.put('/alquileres/:id',putAlquiler) //update
+router.patch('/alquileres/:id',patchAlquiler) //update
+router.delete('/alquileres/:id',deleteAlquiler)
 
 export default router
