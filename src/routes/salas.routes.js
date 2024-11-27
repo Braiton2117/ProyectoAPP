b3ff18106a5654b235321fca584d172112609344
Braiton2117 <richarctm@gmail.com>
2024-11-27 10:14:17 -0500

    Update salas.routes.js

--- src/routes/salas.routes.js
+++ src/routes/salas.routes.js
@@ -18,11 +18,11 @@ const upload=multer({storage});
 const router=Router()
 //armar nuestras rutas
 
-router.get('/salas',verifyToken,getSalas)//select 
-router.get('/salas/:id',verifyToken, getSalaxid)//select con id
-router.post('/salas',verifyToken, upload.single('image'),postSala)//insert
-router.put('/salas/:id',verifyToken, upload.single('image'),putSala) //update
-router.patch('/salas/:id',verifyToken, upload.single('image'),patchSala)  //update
-router.delete('/salas/:id',verifyToken,deleteSala)
+router.get('/salas',getSalas)//select 
+router.get('/salas/:id', getSalaxid)//select con id
+router.post('/salas', upload.single('image'),postSala)//insert
+router.put('/salas/:id', upload.single('image'),putSala) //update
+router.patch('/salas/:id', upload.single('image'),patchSala)  //update
+router.delete('/salas/:id',deleteSala)
 
 export default router
